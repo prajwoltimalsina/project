b94aa4e9ec52e33d37dd5cd63b1b126104d8950e
Prajwol <100836283+prajwoltimalsina@users.noreply.github.com>
2025-05-26 09:28:47 +0545

    commit

--- sw.js
+++ sw.js
@@ -30,3 +30,58 @@ workbox.routing.registerRoute(
     ],
   })
 );
+
+// Service Worker Version
+const CACHE_NAME = 'campaign-manager-v1';
+
+// Files to cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/dashboard.html',
+  '/overview.html',
+  '/auth.js',
+  '/script.js',
+  '/style.css'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
